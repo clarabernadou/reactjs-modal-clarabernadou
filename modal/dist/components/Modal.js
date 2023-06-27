e7d19497cdc251988982f306cfbfb10db31c2d02
Clara Bernadou <clarabernadou56690@gmail.com>
2023-06-27 02:21:11 +0200

    Remove css

--- modal/dist/components/Modal.js
+++ modal/dist/components/Modal.js
@@ -1,81 +1,29 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-require("core-js/modules/es.symbol.js");
-require("core-js/modules/es.symbol.description.js");
-require("core-js/modules/es.symbol.iterator.js");
-require("core-js/modules/es.array.iterator.js");
-require("core-js/modules/es.string.iterator.js");
-require("core-js/modules/web.dom-collections.iterator.js");
-require("core-js/modules/es.array.slice.js");
-require("core-js/modules/es.regexp.to-string.js");
-require("core-js/modules/es.array.from.js");
-require("core-js/modules/es.regexp.exec.js");
-require("core-js/modules/es.regexp.test.js");
-require("core-js/modules/es.weak-map.js");
 require("core-js/modules/es.object.define-property.js");
-require("core-js/modules/es.object.get-own-property-descriptor.js");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = Modal;
-require("core-js/modules/es.object.to-string.js");
-require("core-js/modules/web.dom-collections.for-each.js");
-var _react = _interopRequireWildcard(require("react"));
-require("./styles/modal.css");
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
-function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i.return && (_r = _i.return(), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+var _react = _interopRequireDefault(require("react"));
+var _reactFontawesome = require("@fortawesome/react-fontawesome");
+var _freeSolidSvgIcons = require("@fortawesome/free-solid-svg-icons");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function Modal(_ref) {
   var closeConfirmModal = _ref.closeConfirmModal;
-  // State variable for modal visibility
-  var _useState = (0, _react.useState)(false),
-    _useState2 = _slicedToArray(_useState, 2),
-    isModalOpen = _useState2[0],
-    setIsModalOpen = _useState2[1];
-  (0, _react.useEffect)(function () {
-    if (isModalOpen) {
-      // Disable form inputs and scroll when the modal is open
-      document.body.style.overflow = 'hidden';
-      document.querySelectorAll('input, select, textarea').forEach(function (el) {
-        el.setAttribute('disabled', 'disabled');
-      });
-
-      // Close the confirmation modal with escape
-      document.addEventListener('keydown', handleKeyDown);
-      return function () {
-        document.removeEventListener('keydown', handleKeyDown);
-      };
-    } else {
-      document.body.style.overflow = 'auto';
-      document.querySelectorAll('input, select, textarea').forEach(function (el) {
-        el.removeAttribute('disabled');
-      });
-    }
-  }, [isModalOpen]);
-
-  // Function to close the confirmation modal with escape
-  var handleKeyDown = function handleKeyDown(event) {
-    if (event.key === 'Escape') {
-      closeConfirmModal();
-    }
-  };
-  return /*#__PURE__*/_react.default.createElement("div", {
-    className: "test-modal"
-  }, /*#__PURE__*/_react.default.createElement("button", {
-    onClick: function onClick() {
-      return setIsModalOpen(true);
-    }
-  }, "Test open modal"), isModalOpen && /*#__PURE__*/_react.default.createElement("div", {
-    className: "modal"
-  }, /*#__PURE__*/_react.default.createElement("h2", null, "Confirmation Modal"), /*#__PURE__*/_react.default.createElement("p", null, "This is a confirmation modal."), /*#__PURE__*/_react.default.createElement("button", {
-    onClick: function onClick() {
-      return setIsModalOpen(false);
-    }
-  }, "Close Modal")));
+  return /*#__PURE__*/_react.default.createElement("dialog", {
+    className: "backgroundModal",
+    "aria-modal": "true",
+    "aria-hidden": "true"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    id: "confirmation",
+    className: "modal",
+    role: "dialog"
+  }, /*#__PURE__*/_react.default.createElement("span", null, "Employee Created!"), /*#__PURE__*/_react.default.createElement("button", {
+    onClick: closeConfirmModal,
+    className: "closeBtn",
+    "aria-label": "Close"
+  }, /*#__PURE__*/_react.default.createElement(_reactFontawesome.FontAwesomeIcon, {
+    icon: _freeSolidSvgIcons.faTimes
+  }))));
 }
--- modal/src/lib/components/Modal.jsx
+++ modal/src/lib/components/Modal.jsx
@@ -1,48 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import './styles/modal.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function Modal({ closeConfirmModal }) {
-  // State variable for modal visibility
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    if (isModalOpen) {
-      // Disable form inputs and scroll when the modal is open
-      document.body.style.overflow = 'hidden';
-      document.querySelectorAll('input, select, textarea').forEach((el) => {
-        el.setAttribute('disabled', 'disabled');
-      });
-
-      // Close the confirmation modal with escape
-      document.addEventListener('keydown', handleKeyDown);
-      return () => {
-        document.removeEventListener('keydown', handleKeyDown);
-      };
-    } else {
-      document.body.style.overflow = 'auto';
-      document.querySelectorAll('input, select, textarea').forEach((el) => {
-        el.removeAttribute('disabled');
-      });
-    }
-  }, [isModalOpen]);
-
-  // Function to close the confirmation modal with escape
-  const handleKeyDown = (event) => {
-    if (event.key === 'Escape') {
-      closeConfirmModal();
-    }
-  };
-
   return (
-    <div className='test-modal'>
-      <button onClick={() => setIsModalOpen(true)}>Test open modal</button>
-      {isModalOpen && (
-        <div className='modal'>
-          <h2>Confirmation Modal</h2>
-          <p>This is a confirmation modal.</p>
-          <button onClick={() => setIsModalOpen(false)}>Close Modal</button>
-        </div>
-      )}
-    </div>
+    <dialog className="backgroundModal" aria-modal="true" aria-hidden="true">
+      <div id="confirmation" className="modal" role="dialog">
+        <span>Employee Created!</span>
+        <button
+          onClick={closeConfirmModal}
+          className="closeBtn"
+          aria-label="Close"
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+      </div>
+    </dialog>
   );
 }
